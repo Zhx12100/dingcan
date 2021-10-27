867193b2ce497a2f5344a3a469f6af7c392ac78f
张恒信 <chenlunpingzi@163.com>
2021-10-27 17:02:03 +0800

    12

--- dingcan/pages/Administration/index/index.js
+++ dingcan/pages/Administration/index/index.js
@@ -1,15 +1,30 @@
 import * as echarts from '../../../ec-canvas/echarts';
-let chartPie1=1;
-function getOption(data) {
+let chartPie1 = 1;
+let chartPie2 = 1;
+let chartPie3 = 1;
+
+function getOption(data, top) {
+  let count = 0;
+  data.forEach((v,i) =>{
+    count = v.value+count
+  })
   var option = {
     tooltip: {
-      trigger: 'item'
+      trigger: 'item',
+      formatter: "{a} <br/>{b}: {c} ({d}%)",
     },
+    disableTouch: true,
     legend: {
       bottom: '5%',
       left: 'center',
       formatter: function (name) {
-        return '{a|' + name + '}'
+        var val
+        data.forEach((v, i) => {
+          if (v.name == name) {
+            val = v.value
+          }
+        })
+        return '{a|' + name + ' ' + val + '}'
       },
       textStyle: {
         backgroundColor: "transparent", // 文字块背景色，一定要加上，否则对齐不会生效
@@ -23,27 +38,53 @@ function getOption(data) {
     series: [{
       name: 'Access From',
       type: 'pie',
-      center: ['50%', '135'],
-      radius: ['40%', '70%'],
-      avoidLabelOverlap: false,
+      center: ['50%', top],
+      radius: ['30%', '50%'],
+      avoidLabelOverlap: true,
       itemStyle: {
         borderRadius: 10,
         borderColor: '#fff',
         borderWidth: 2
       },
-      label: {
-        show: false,
-        position: 'center'
-      },
+      // label: {
+      //   show: false,
+      //   position: 'center'
+      // },
       emphasis: {
         label: {
-          show: false,
+          show: true,
           fontSize: '40',
           fontWeight: 'bold'
         }
       },
+      label: {
+        alignTo: 'edge',
+        // formatter: '{name|{b}}\n{time|''}',
+        formatter:function (params) {
+          // var val;
+          // data.forEach((v, i) => {
+          //   if (v.name == params.seriesName) {
+          //     val = v.value
+          //   }
+          // })
+          return `${params.name}\n${params.value} ${(Math.round(params.value/count * 10000) / 100.00)}%`
+        },
+        minMargin: 5,
+        edgeDistance: 10,
+        lineHeight: 15,
+        rich: {
+          time: {
+            fontSize: 10,
+            color: '#999'
+          }
+        }
+      },
       labelLine: {
-        show: false
+        normal: {
+          show: true,
+          length: 5,
+          length2: 10
+        }
       },
       data: data
     }]
@@ -64,27 +105,30 @@ Component({
       start_date: '',
       end_date: '',
     },
-    form1Height:400,
+    form1Height: 400,
     //加班餐工作日
     formData2: {
       start_date: '',
       end_date: '',
     },
+    form2Height: 400,
     //加班餐节假日
     formData3: {
       start_date: '',
       end_date: '',
     },
+    form3Height: 400,
     ec1: {
-      
       lazyLoad: true
     },
     ec2: {
-      onInit: '',
+      lazyLoad: true
     },
     ec3: {
-      onInit: '',
+      lazyLoad: true
     },
+    isIOS:false,
+    switch1Checked: true,
   },
 
   lifetimes: {
@@ -122,8 +166,16 @@ Component({
         ['formData3.end_date']: end_date,
       })
       console.log(that.data.formData1)
-      this.echartsComponnet1 = this.selectComponent('#mychart-dom-pie1');
+      that.getOt()
       that.getForm1()
+      that.getForm2()
+      that.getForm3()
+      let res = wx.getSystemInfoSync();
+      console.log(res)
+      if(res.platform == 'ios') {
+        this.setData({ isIOS: true })
+      }
+      console.log('ios',this.data.isIOS)
     },
     moved: function () {
       console.log('moved')
@@ -140,7 +192,7 @@ Component({
   },
   methods: {
     //表格1
-    getForm1(){
+    getForm1() {
       let that = this
       let data = that.data.formData1
       data.meal_type = 1
@@ -154,87 +206,268 @@ Component({
         success: function (res) {
           console.log('统计数据', res.data.data)
           wx.hideToast();
-          that.setData({
-            detail: res.data.data
-          })
           console.log(chartPie1)
-          
-          
-          that.onInit1()
-          
+          that.onInit1(res.data.data)
+        }
+      })
+    },
+    //表格2
+    getForm2() {
+      let that = this
+      let data = that.data.formData2
+      data.meal_type = 2
+      wx.request({
+        url: app.globalData.baseUrl + '/applet/meal/manager/index_data',
+        method: "post",
+        header: {
+          'token': app.globalData.token
+        },
+        data: data,
+        success: function (res) {
+          console.log('统计数据', res.data.data)
+          wx.hideToast();
+          that.onInit2(res.data.data)
+        }
+      })
+    },
+    //表格3
+    getForm3() {
+      let that = this
+      let data = that.data.formData3
+      data.meal_type = 3
+      wx.request({
+        url: app.globalData.baseUrl + '/applet/meal/manager/index_data',
+        method: "post",
+        header: {
+          'token': app.globalData.token
+        },
+        data: data,
+        success: function (res) {
+          console.log('统计数据', res.data.data)
+          wx.hideToast();
+          that.onInit3(res.data.data)
         }
       })
     },
 
-    onInit1: function (){
+    onInit1: function (data) {
+      var data = data
+      // var data = [{
+      //     value: 1048,
+      //     name: '区纪委监委机关'
+      //   },
+      //   {
+      //     value: 735,
+      //     name: '区纪委监委机关1'
+      //   },
+      //   {
+      //     value: 580,
+      //     name: '区机关事务管2理局'
+      //   },
+      //   {
+      //     value: 484,
+      //     name: '区机关事3务管理局'
+      //   },
+      //   {
+      //     value: 300,
+      //     name: '区机关事4务管理局'
+      //   },
+      //   {
+      //     value: 300,
+      //     name: '区机关事4务管3理局'
+      //   },
+      //   {
+      //     value: 300,
+      //     name: '区机关事4务3管理局'
+      //   },
+      //   {
+      //     value: 300,
+      //     name: '区机关事4务3管理局'
+      //   },
+      //   {
+      //     value: 300,
+      //     name: '区机关事4务3管理局'
+      //   }
+      // ]
+      var height;
+      if (data.length <= 2) {
+        height = 200
+      } else {
+        height = 200 + Math.ceil((data.length - 2) / 2) * 60
+      }
+      this.setData({
+        form1Height: height
+      })
+      this.echartsComponnet1 = this.selectComponent('#mychart-dom-pie1');
+      const getPixelRatio = () => {
+        let pixelRatio = 0
+        wx.getSystemInfo({
+          success: function (res) {
+            pixelRatio = res.pixelRatio
+          },
+          fail: function () {
+            pixelRatio = 0
+          }
+        })
+        return pixelRatio
+      }
+      var dpr = getPixelRatio()
       this.echartsComponnet1.init((canvas, width, height) => {
         //初始化echarts元素，绑定到全局变量，方便更改数据
         chartPie1 = echarts.init(canvas, null, {
           width: width,
-          height: height
+          height: height,
+          devicePixelRatio: dpr
         });
-        // canvas.setChart(chartPie1);
-        chartPie1.resize()
-        var data = [
-          {
-            value: 1048,
-            name: '区纪委监委机关'
-          },
-          {
-            value: 735,
-            name: '区纪委监委机关1'
-          },
-          {
-            value: 580,
-            name: '区机关事务管2理局'
-          },
-          {
-            value: 484,
-            name: '区机关事3务管理局'
-          },
-          {
-            value: 300,
-            name: '区机关事4务管理局'
-          },
-          {
-            value: 300,
-            name: '区机关事4务管3理局'
-          },
-          {
-            value: 300,
-            name: '区机关事4务3管理局'
-          },
-          {
-            value: 300,
-            name: '区机关事4务3管理局'
-          },
-          {
-            value: 300,
-            name: '区机关事4务3管理局'
-          }
-        ]
-        var height;
-        if(data.length<=2){
-          height=200
-        }else{
-          height=200+Math.ceil((data.length-2)/2)*100
-        }
-        this.setData({
-          form1Height: height
-        })
-        chartPie1.setOption(getOption(data))
-        chartPie1.resize()
+        chartPie1.setOption(getOption(data, height / 3))
       })
       return chartPie1
-      
-      // chartPie1.resize();
-      // chartPie1.resize({height:this.form1Height})
-
-      //可以先不setOption，等数据加载好后赋值，
-      //不过那样没setOption前，echats元素是一片空白，体验不好，所有我先set。
-      // var xData = [1,2,3,4,5......];  // x轴数据 自己写
-      // var option = getOption(xData);
-      // chartLine.setOption(option);
+    },
+    onInit2: function (data) {
+      var data = data
+      // var data = [{
+      //     value: 1048,
+      //     name: '区纪委监委机关'
+      //   },
+      //   {
+      //     value: 735,
+      //     name: '区纪委监委机关1'
+      //   },
+      //   {
+      //     value: 580,
+      //     name: '区机关事务管2理局'
+      //   },
+      //   {
+      //     value: 484,
+      //     name: '区机关事3务管理局'
+      //   },
+      //   {
+      //     value: 300,
+      //     name: '区机关事4务管理局'
+      //   },
+      //   {
+      //     value: 300,
+      //     name: '区机关事4务管3理局'
+      //   },
+      //   {
+      //     value: 300,
+      //     name: '区机关事4务3管理局'
+      //   },
+      //   {
+      //     value: 300,
+      //     name: '区机关事4务3管理局'
+      //   },
+      //   {
+      //     value: 300,
+      //     name: '区机关事4务3管理局'
+      //   }
+      // ]
+      var height;
+      if (data.length <= 2) {
+        height = 200
+      } else {
+        height = 200 + Math.ceil((data.length - 2) / 2) * 60
+      }
+      this.setData({
+        form2Height: height
+      })
+      this.echartsComponnet2 = this.selectComponent('#mychart-dom-pie2');
+      const getPixelRatio = () => {
+        let pixelRatio = 0
+        wx.getSystemInfo({
+          success: function (res) {
+            pixelRatio = res.pixelRatio
+          },
+          fail: function () {
+            pixelRatio = 0
+          }
+        })
+        return pixelRatio
+      }
+      var dpr = getPixelRatio()
+      this.echartsComponnet2.init((canvas, width, height) => {
+        //初始化echarts元素，绑定到全局变量，方便更改数据
+        chartPie2 = echarts.init(canvas, null, {
+          width: width,
+          height: height,
+          devicePixelRatio: dpr
+        });
+        chartPie2.setOption(getOption(data, height / 3))
+      })
+      return chartPie2
+    },
+    onInit3: function (data) {
+      var data = data
+      // var data = [{
+      //     value: 1048,
+      //     name: '区纪委监委机关'
+      //   },
+      //   {
+      //     value: 735,
+      //     name: '区纪委监委机关1'
+      //   },
+      //   {
+      //     value: 580,
+      //     name: '区机关事务管2理局'
+      //   },
+      //   {
+      //     value: 484,
+      //     name: '区机关事3务管理局'
+      //   },
+      //   {
+      //     value: 300,
+      //     name: '区机关事4务管理局'
+      //   },
+      //   {
+      //     value: 300,
+      //     name: '区机关事4务管3理局'
+      //   },
+      //   {
+      //     value: 300,
+      //     name: '区机关事4务3管理局'
+      //   },
+      //   {
+      //     value: 300,
+      //     name: '区机关事4务3管理局'
+      //   },
+      //   {
+      //     value: 300,
+      //     name: '区机关事4务3管理局'
+      //   }
+      // ]
+      var height;
+      if (data.length <= 2) {
+        height = 200
+      } else {
+        height = 200 + Math.ceil((data.length - 2) / 2) * 60
+      }
+      this.setData({
+        form3Height: height
+      })
+      this.echartsComponnet3 = this.selectComponent('#mychart-dom-pie3');
+      const getPixelRatio = () => {
+        let pixelRatio = 0
+        wx.getSystemInfo({
+          success: function (res) {
+            pixelRatio = res.pixelRatio
+          },
+          fail: function () {
+            pixelRatio = 0
+          }
+        })
+        return pixelRatio
+      }
+      var dpr = getPixelRatio()
+      this.echartsComponnet3.init((canvas, width, height) => {
+        //初始化echarts元素，绑定到全局变量，方便更改数据
+        chartPie3 = echarts.init(canvas, null, {
+          width: width,
+          height: height,
+          devicePixelRatio: dpr
+        });
+        chartPie3.setOption(getOption(data, height / 3))
+      })
+      return chartPie3
     },
 
     bindDateChangeStart1: function (e) {
@@ -242,13 +475,79 @@ Component({
       this.setData({
         ['formData1.start_date']: e.detail.value,
       })
+      this.getForm1()
     },
     bindDateChangeEnd1: function (e) {
       console.log('picker发送选择改变，携带值为', e.detail.value)
       this.setData({
         ['formData1.end_date']: e.detail.value,
       })
+      this.getForm1()
     },
+    bindDateChangeStart2: function (e) {
+      console.log('picker发送选择改变，携带值为', e.detail.value)
+      this.setData({
+        ['formData2.start_date']: e.detail.value,
+      })
+      this.getForm2()
+    },
+    bindDateChangeEnd2: function (e) {
+      console.log('picker发送选择改变，携带值为', e.detail.value)
+      this.setData({
+        ['formData2.end_date']: e.detail.value,
+      })
+      this.getForm2()
+    },
+    bindDateChangeStart3: function (e) {
+      console.log('picker发送选择改变，携带值为', e.detail.value)
+      this.setData({
+        ['formData3.start_date']: e.detail.value,
+      })
+      this.getForm3()
+    },
+    bindDateChangeEnd3: function (e) {
+      console.log('picker发送选择改变，携带值为', e.detail.value)
+      this.setData({
+        ['formData3.end_date']: e.detail.value,
+      })
+      this.getForm3()
+    },
+
+    getOt: function (){
+      let that = this
+      wx.request({
+        url: app.globalData.baseUrl + '/system/get_ot',
+        method: "post",
+        header: {
+          'token': app.globalData.token
+        },
+        data: {},
+        success: function (res) {
+          console.log('节假日加班餐', res.data.data)
+          that.setData({
+            switch1Checked:res.data.data.ot_holiday_flag
+          })
+        }
+      })
+    },
+    switch1Change: function (e) {
+      console.log('picker发送选择改变，携带值为', e.detail.value)
+      this.setData({
+        switch1Checked: e.detail.value,
+      })
+      wx.request({
+        url: app.globalData.baseUrl + '/system/applet/set_ot',
+        method: "post",
+        header: {
+          'token': app.globalData.token
+        },
+        data: {ot_holiday_flag:e.detail.value},
+        success: function (res) {
+          console.log('设置', res.data.data)
+        }
+      })
+    },
+
   },
 
   /**
